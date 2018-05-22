f5fb159b3d64a2b06c90637d123304bc723d88bc
Duc Vuong <duc.vuong@sutrixsolutions.com>
2018-05-22 18:11:32 +0700

    added dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { WordWrapComponent } from './app-word/word.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +16,13 @@ import { MessagesComponent } from './messages/messages.component';
     WordWrapComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import {Hero} from './heroes/hero'
 import {Heroes} from './heroes/heroes'
+import {MessageService} from './message.service'
 import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
   //normal for mockup data
+ 
+  constructor(private messageService:MessageService) { }
   getHeroes() : Hero[] {
+    
     return Heroes
   }
   // using observable
   getHeroesByObservable() : Observable<Hero[]> {
+    this.messageService.add('Duc added');
     return of(Heroes);
   }
-  constructor() { }
 }
